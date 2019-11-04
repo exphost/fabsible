74fec5a46684a65ef9272bba0e1dea18c2944beb
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2019-11-04 21:23:29 +0100

    basic package information
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>
    Change-Id: I9f3d604059dbdd1cd0accac4aac2ae06651b037d

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# fabsible
+Fabsible
+========
+
+Fabsible is ansible framework which is object-oriented.
+
+Init
+----
+
+To init new project, type `fabsible-init -i <inventory_file>` to init new project
+
+Configuration
+-------------
+
+TODO
+
+Mostly typical ansible configuration
+
+
+Run
+---
+
+`fabsible-play`
--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import setuptools
 from pathlib import Path
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 def get_data_files():
     files = [ ("fabsible/" + "/".join(x.parent.parts[3:]), ["/".join(x.parts)]) for x in Path('lib/fabsible/data').glob('**/*') if x.is_file() and ('/.git/' not in str(x) and not str(x).endswith('.git'))]
     return files
@@ -10,8 +13,8 @@ setuptools.setup(
     version="0.0.1",
     author="TORGiren",
     author_email="exphost@fabrykowski.pl",
-    description="A small example package",
-    long_description="long description...",
+    description="Ansible framework using object-oriented configuration",
+    long_description=read('README.md'),
     long_description_content_type="text/markdown",
     url="https://github.com/exphost/fabsible",
     package_dir={'':'lib'},
@@ -19,7 +22,15 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: POSIX",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Information Technology",
+        "Natural Language :: English",
+        "Topic :: System :: Systems Administration",
+        "Topic :: System :: Installation/Setup",
+        "Development Status :: 3 - Alpha",
     ],
     python_requires='>=3.6',
     install_requires=[
