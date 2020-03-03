8426be46782ad8dca6fd275dbf2d593ec0e1afaf
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2020-03-03 09:42:38 +0100

    v0.0.6
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>
    Change-Id: I5e100c96bbc5125f998f0287d9b34274a8c30c1c

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_data_files():
 
 setuptools.setup(
     name="fabsible",
-    version="0.0.4",
+    version="0.0.6",
     author="TORGiren",
     author_email="exphost@fabrykowski.pl",
     description="Ansible framework using object-oriented configuration",
